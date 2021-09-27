fb767a3036a308f30dc19bfdb9891b4c410d09c9
alexrenk <qb.alex144@gmail.com>
2021-09-27 23:09:57 +0300

    test1

--- Assets/Scripts/PlayerHeadController.cs
+++ Assets/Scripts/PlayerHeadController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerHeadController : MonoBehaviour
 {
+    public float click_delay = 1;
+    float current_delay = 0;
+
     Transform headTransform;
     PlayerController playerController;
     int layermask = 1 << 8;
@@ -23,13 +26,26 @@ public class PlayerHeadController : MonoBehaviour
         headTransform.Rotate(0, 0, vertical, Space.Self);
         playerController.faceDirection = Quaternion.Euler(0, horizontal, 0) * playerController.faceDirection;
 
-        if(Input.GetAxis("Fire1")!=0)
+        if(current_delay < Time.deltaTime) { current_delay = 0; }
+        else { current_delay -= Time.deltaTime; }
+
+        if(((Input.GetAxis("Fire1")!=0) || (Input.GetAxis("Fire2") != 0)) && current_delay == 0)
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, Mathf.Infinity, layermask))
             {
                 Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * hit.distance, Color.yellow);
                 Debug.Log("Did Hit: "+ hit.transform.name);
+                Swap_mat click = hit.transform.gameObject.GetComponent<Swap_mat>();
+                if (Input.GetAxis("Fire1") != 0)
+                {
+                    click.DoAction(Swap_mat.Actions.LEFT);
+                }
+                else
+                {
+                    click.DoAction(Swap_mat.Actions.RIGHT);
+                }
+                current_delay = click_delay;
             }
             else
             {
